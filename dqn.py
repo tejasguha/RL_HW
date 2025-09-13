890474727280c03e247481703bf18f604cb75f37
roofTiles <guhatejas@gmail.com>
2025-09-12 20:04:36 -0400

    hw2 init commit

---
+++ dqn.py
@@ -0,0 +1,141 @@
+#! python3
+
+import argparse
+import collections
+import random
+
+import gymnasium as gym
+import matplotlib.pyplot as plt
+import numpy as np # NOTE only imported because https://github.com/pytorch/pytorch/issues/13918
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+
+
+class ReplayMemory():
+    def __init__(self, memory_size, batch_size):
+        # define init params
+        # use collections.deque
+        # BEGIN STUDENT SOLUTION
+        # END STUDENT SOLUTION
+        pass
+
+
+    def sample_batch(self):
+        # randomly chooses from the collections.deque
+        # BEGIN STUDENT SOLUTION
+        # END STUDENT SOLUTION
+        pass
+
+
+    def append(self, transition):
+        # append to the collections.deque
+        # BEGIN STUDENT SOLUTION
+        # END STUDENT SOLUTION
+        pass
+
+
+
+class DeepQNetwork(nn.Module):
+    def __init__(self, state_size, action_size, double_dqn, lr_q_net=2e-4, gamma=0.99, epsilon=0.05, target_update=50, burn_in=10000, replay_buffer_size=50000, replay_buffer_batch_size=32, device='cpu'):
+        super(DeepQNetwork, self).__init__()
+
+        # define init params
+        self.state_size = state_size
+        self.action_size = action_size
+        self.double_dqn = double_dqn
+
+        self.gamma = gamma
+        self.epsilon = epsilon
+
+        self.target_update = target_update
+
+        self.burn_in = burn_in
+
+        self.device = device
+
+        hidden_layer_size = 256
+
+        # q network
+        q_net_init = lambda: nn.Sequential(
+            nn.Linear(state_size, hidden_layer_size),
+            nn.ReLU(),
+            # BEGIN STUDENT SOLUTION
+            # END STUDENT SOLUTION
+        )
+
+        # initialize replay buffer, networks, optimizer, move networks to device
+        # BEGIN STUDENT SOLUTION
+        # END STUDENT SOLUTION
+
+
+    def forward(self, state, new_state):
+        # calculate q value and target
+        # use the correct network for the target based on self.double_dqn
+        # BEGIN STUDENT SOLUTION
+        # END STUDENT SOLUTION
+
+
+    def get_action(self, state, stochastic):
+        # if stochastic, sample using epsilon greedy, else get the argmax
+        # BEGIN STUDENT SOLUTION
+        # END STUDENT SOLUTION
+        pass
+
+
+
+def graph_agents(
+    graph_name, mean_undiscounted_returns, test_frequency, max_steps, num_episodes
+):
+    print(f'Starting: {graph_name}')
+
+    # graph the data mentioned in the homework pdf
+    # BEGIN STUDENT SOLUTION
+    # END STUDENT SOLUTION
+
+    # plot the total rewards
+    xs = [i * test_frequency for i in range(len(average_total_rewards))]
+    fig, ax = plt.subplots()
+    plt.fill_between(xs, min_total_rewards, max_total_rewards, alpha=0.1)
+    ax.plot(xs, average_total_rewards)
+    ax.set_ylim(-max_steps * 0.01, max_steps * 1.1)
+    ax.set_title(graph_name, fontsize=10)
+    ax.set_xlabel('Episode')
+    ax.set_ylabel('Average Total Reward')
+    fig.savefig(f'./graphs/{graph_name}.png')
+    plt.close(fig)
+    print(f'Finished: {graph_name}')
+
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Train an agent.')
+    parser.add_argument('--num_runs', type=int, default=5, help='Number of runs to average over for graph')
+    parser.add_argument('--num_episodes', type=int, default=1000, help='Number of episodes to train for')
+    parser.add_argument('--max_steps', type=int, default=200, help='Maximum number of steps in the environment')
+    parser.add_argument('--env_name', type=str, default='CartPole-v1', help='Environment name')
+    parser.add_argument(
+        "--test_frequency",
+        type=int,
+        default=100,
+        help="Number of training episodes between test episodes",
+    )
+    parser.add_argument("--double_dqn", action="store_true", help="Use Double DQN")
+    return parser.parse_args()
+
+
+
+def main():
+    args = parse_args()
+
+    # init args, agents, and call graph_agent on the initialized agents
+    # BEGIN STUDENT SOLUTION
+    # END STUDENT SOLUTION
+
+
+
+if '__main__' == __name__:
+    main()
+
